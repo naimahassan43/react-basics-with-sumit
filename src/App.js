eeccb20c278ae5170b512fb5b4920438f6fef11b
Naima Hassan <naima.hassan434@gmail.com>
2021-09-09 21:27:21 +0100

    Functional Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
+
 function App() {
-  return <div>Hello world</div>;
+  return <div>Hello</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 // import App from './App';
 
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
-setInterval(() => {
-  const element=(
-    <h1 className="heading">
-      <span>The time is {new Date().toLocaleTimeString()}</span>
-    </h1>
-  );
+import ReactDOM from 'react-dom';
 
-  ReactDOM.render(element, document.getElementById('root'));
-}, 1000);
+function Clock({locale}) {
+    return (
+      <h1 className="heading">
+        <span className="text">Hello, {new Date().toLocaleTimeString(locale)}</span>
+      </h1>
+    )
+}
 
+ReactDOM.render(<Clock locale="bn-BD" />, document.getElementById('root'));
 
 
 
