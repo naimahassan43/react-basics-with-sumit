de83935cb376c91be289254dd52c7a2df76bf088
Naima Hassan <naima.hassan434@gmail.com>
2021-09-09 13:52:48 +0100

    Rendering Elements

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+setInterval(() => {
+  const element=(
+    <h1 className="heading">
+      <span>The time is {new Date().toLocaleTimeString()}</span>
+    </h1>
+  );
+
+  ReactDOM.render(element, document.getElementById('root'));
+}, 1000);
+
+
+
+
+
